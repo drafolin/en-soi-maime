7d74b1638b1f40b1544514bf293a395d45cdcdb6
Odin Beuchat <odin.beuchat@divtec.ch>
2022-12-07 09:02:41 +0100

    added header layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import favicon from "./assets/favicon.jpg";
 
 const Layout = () => (
 	<>
 		<header>
+			<Link to="/">
+				<img
+					src={favicon}
+					alt="En soi m'aime"
+				/>
+				En soi m'aime
+			</Link>
 			<nav>
 				<ul>
-
+					<li>
+						<Link to={"/"}>
+							Accueil
+						</Link>
+					</li>
 				</ul>
 			</nav>
 		</header>
