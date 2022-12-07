4eddfea8a0118ce86987f5f225d4250fbe9fc912
Odin Beuchat <odin.beuchat@divtec.ch>
2022-12-07 09:20:13 +0100

    created footer

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,8 @@
 import { Outlet, Link } from "react-router-dom";
 import favicon from "./assets/favicon.jpg";
+import reactIcon from "./assets/react.svg";
+import viteIcon from "./assets/vite.svg";
+import heart from "./assets/heart.svg";
 
 const Layout = () => (
 	<>
@@ -27,7 +30,17 @@ const Layout = () => (
 		</main>
 
 		<footer>
-
+			<div>
+				Made with <Link to="https://reactjs.org"><img src={reactIcon} alt="Reactjs" /></Link>
+				, <Link to="https://vitejs.org"><img src={viteIcon} alt="Vitejs" /></Link>
+				, and <img src={heart} alt="love" />
+			</div>
+			<div>
+				Created by <Link to="https://dindin.ch">dindin|nibnib</Link>
+			</div>
+			<div>
+				&copy; en soi m'aime, 2022-2022
+			</div>
 		</footer>
 	</>
 );
